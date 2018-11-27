07e92eb7132bf4335f8845395fdf762f12cb8efa
RaghunathVadlakonda <vadlakonda.raghu5@gmail.com>
2018-11-27 18:31:09 +0530

    updated files

---
+++ public/chat.js
@@ -0,0 +1,31 @@
+// connection for front-end
+
+
+const socket = io.connect('http://localhost:9000');
+
+ const message = document.getElementById('message'),
+       handle = document.getElementById('handle'),
+       btn = document.getElementById('send'),
+       output = document.getElementById('output'),
+       feedback = document.getElementById('feedback');
+
+
+btn.addEventListener('click',() => {
+    socket.emit('chat',{
+        message:message.value,
+        handle:handle.value
+    });
+});
+
+message.addEventListener('keypress',() => {
+    socket.emit('typing', handle.value);
+});
+
+socket.on('chat', (data) => {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message </em> </p>';
+});
